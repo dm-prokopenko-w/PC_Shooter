6806c918e2f467664f2aa00d10fbd28a811b9c56
Dmytro Prokopenko <dm.prokopenko.w@gmail.com>
2023-11-04 22:37:57 +0200

    Added ui scripts

---
+++ Assets/Scripts/UI/BtnClearCharacterView.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using Game.Core;
+using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+namespace Game.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class BtnClearCharacterView : MonoBehaviour
+    {
+        [Inject] private InjectController _injectController;
+
+        [SerializeField] private Button _button;
+
+        private void Awake()
+        {
+            _injectController.AddUIItem(new InjectItem(Constants.ClearCharacterViewBtn, _button));
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BtnSceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+using Game.Core;
+using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+namespace Game.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class BtnSceneLoader : MonoBehaviour
+    {
+        [Inject] private InjectController _injectController;
+
+        [SerializeField] private int _sceneIndex;
+        [SerializeField] private Button _button;
+
+        private void Awake()
+        {
+            _injectController.AddUIItem(new InjectItem(Constants.SceneLoaderBtn, _button, _sceneIndex));
+        }
+    }
+}
